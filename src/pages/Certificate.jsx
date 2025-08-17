2f33db8a48f2ebb2b0301864971ca8a108b41132
deflayy <ipututheflyarnawa@gmail.com>
2025-08-17 17:49:23 +0700

    Update Sertifikat Gambar

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -1,42 +1,49 @@
 import React from "react";
 import "../css/Certificate.css";
-import serti from "../assets/serti.jpg"; // ✅ Import gambar
+
+// ✅ Import semua gambar sertifikat
+import serti1 from "../assets/serti1.jpg";
+import serti2 from "../assets/serti2.jpg";
+import serti3 from "../assets/serti3.jpg";
+import serti4 from "../assets/serti4.jpg";
+import serti5 from "../assets/serti5.jpg";
+import serti6 from "../assets/serti6.jpg";
 
 const certificates = [
   {
     id: 1,
     title: "Dicoding React Developer 1",
-    image: serti,
+    image: serti1,
     link: "https://www.dicoding.com/certificates/XXXXXX",
   },
   {
     id: 2,
     title: "Dicoding React Developer 2",
-    image: serti,
+    image: serti2,
     link: "https://www.dicoding.com/certificates/XXXXXX",
   },
   {
     id: 3,
     title: "Dicoding React Developer 3",
-    image: serti,
+    image: serti3,
     link: "https://www.dicoding.com/certificates/XXXXXX",
   },
   {
     id: 4,
     title: "Belajar Dasar Data 4",
-    image: serti,
+    image: serti4,
     link: "https://www.dicoding.com/certificates/YYYYYY",
   },
   {
     id: 5,
-    title: "Belajar Dasar Data 4",
-    image: serti,
+    title: "Belajar Dasar Data 5",
+    image: serti5,
     link: "https://www.dicoding.com/certificates/YYYYYY",
   },
   {
     id: 6,
-    title: "Belajar Dasar Data 4",
-    image: serti,
+    title: "Belajar Dasar Data 6",
+    image: serti6,
     link: "https://www.dicoding.com/certificates/YYYYYY",
   },
 ];
